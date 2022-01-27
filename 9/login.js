7a18db95c119ee88b7b0930ea662b8436490ede5
afifahpertiwi <92978153+afifahpertiwi@users.noreply.github.com>
2022-01-27 20:59:25 +0700

    Add files via upload

---
+++ 9/login.js
@@ -0,0 +1,15 @@
+function startlog()
+    {
+        let username =  document.getElementById("username").value;
+        let password =  document.getElementById("password").value; 
+        let internet1 =  ("fah") + ( "131313");
+
+        if ( username === "fah" &&  password ==="131313"){
+        let konfirmasi = document.getElementById("akses").innerHTML = "login seccess"
+        window.alert ("selamat anda berhasil login!")
+    }
+
+    else {
+    let. konfirmasi = document.getElementById("akses").innerHTML= "gagal login"
+    }
+}
